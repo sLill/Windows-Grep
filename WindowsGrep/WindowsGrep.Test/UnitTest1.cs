1cf82035d964ee2a614b393e355399018d2c141f
Samuel Turner-Lill <STurner-Lill@PioneerRx.com>
2020-10-01 14:05:22 -0500

    - Test project added

---
+++ WindowsGrep/WindowsGrep.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace WindowsGrep.Test
+{
+    public class Tests
+    {
+        #region Setup..
+        [SetUp]
+        public void Setup()
+        {
+        }
+        #endregion Setup..
+
+        #region Tests..
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        } 
+        #endregion Tests..
+    }
+}
