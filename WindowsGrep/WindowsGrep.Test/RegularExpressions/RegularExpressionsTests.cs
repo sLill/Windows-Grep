e7b771acbcddf24f121dd55e90ac8d99a56a1cca
Samuel Turner-Lill <STurner-Lill@PioneerRx.com>
2020-10-05 14:16:58 -0500

    -

---
+++ WindowsGrep/WindowsGrep.Test/RegularExpressions/RegularExpressionsTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WindowsGrep.Common;
+using WindowsGrep.Engine;
+
+namespace WindowsGrep.Test.RegularExpressions
+{
+    public class RegularExpressionsTests : TestBase
+    {
+        #region Member Variables..
+        private string _TestDataRelativePath = @"RegularExpressions\TestData";
+        #endregion Member Variables..
+
+        #region Properties..
+        #endregion Properties..
+
+        #region Setup
+        [SetUp]
+        public void Setup()
+        {
+            TestDataDirectory = Path.Combine(TestConfigurationManager.ProjectDirectory, _TestDataRelativePath);
+        }
+        #endregion Setup
+
+        #region Tests..
+        #region RegularExpressions_One
+        [Test]
+        public void RegularExpressions_One()
+        {
+       
+        }
+        #endregion RegularExpressions_One 
+        #endregion Tests..
+    }
+}
