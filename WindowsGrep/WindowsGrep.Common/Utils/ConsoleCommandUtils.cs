36a44057be3d934b50a61317a6360404cece558f
sLill <SamuelTurnerLill@gmail.com>
2023-03-09 13:54:33 -0600

    - Reduced overall complexity of GrepEngine

---
+++ WindowsGrep/WindowsGrep.Common/Utils/ConsoleCommandUtils.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WindowsGrep.Common
+{
+    public static class ConsoleCommandUtils
+    {
+        #region Fields..
+        private const string FILE_SIZE_PATTERN = @"(?<Size>\d+)(?<SizeType>\S{2})?";
+
+        private static Regex _fileSizeRegex = new Regex(FILE_SIZE_PATTERN);
+        #endregion Fields..
+
+        #region Methods..
+        #region GetFileSizeMaximum
+        public static long GetFileSizeMaximum(ConsoleCommand consoleCommand)
+        {
+            long fileSizeMaximum = -1;
+            bool fileSizeMaximumFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.FileSizeMaximum);
+
+            if (fileSizeMaximumFlag)
+            {
+                try
+                {
+                    string fileSizeMaximumParameter = consoleCommand.CommandArgs[ConsoleFlag.FileSizeMaximum];
+
+                    var match = _fileSizeRegex.Match(fileSizeMaximumParameter);
+                    long size = Convert.ToInt64(match.Groups["Size"].Value);
+
+                    if (size < 0)
+                        throw new Exception("Error: Size parameter cannot be less than 0");
+
+                    long fileSizeModifier = FileSizeType.Kb.GetCustomAttribute<ValueAttribute>().Value;
+
+                    string sizeType = match.Groups["SizeType"].Value.ToUpper();
+                    if (!sizeType.IsNullOrEmpty())
+                        fileSizeModifier = Enum.Parse<FileSizeType>(sizeType, true).GetCustomAttribute<ValueAttribute>().Value;
+
+                    fileSizeMaximum = size * fileSizeModifier;
+                }
+                catch
+                {
+                    throw new Exception($"Error: Could not parse filesize parameter" +
+                        $"{Environment.NewLine}Format should follow [SIZE] or [SIZE][TYPE]. Acceptable TYPE parameters are kb, mb, gb, tb" +
+                        $"{Environment.NewLine}For more information, visit https://github.com/sLill/Windows-BudgetGrep/wiki/WindowsGrep.CommandFlags");
+                }
+            }
+
+            return fileSizeMaximum;
+        }
+        #endregion GetFileSizeMaximum
+
+        #region GetFileSizeMinimum
+        public static long GetFileSizeMinimum(ConsoleCommand consoleCommand)
+        {
+            long fileSizeMinimum = -1;
+            bool fileSizeMinimumFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.FileSizeMinimum);
+
+            if (fileSizeMinimumFlag)
+            {
+                try
+                {
+                    string fileSizeMinimumParameter = consoleCommand.CommandArgs[ConsoleFlag.FileSizeMinimum];
+
+                    var match = _fileSizeRegex.Match(fileSizeMinimumParameter);
+                    long size = Convert.ToInt64(match.Groups["Size"].Value);
+
+                    if (size < 0)
+                    {
+                        throw new Exception("Error: Size parameter cannot be less than 0");
+                    }
+
+                    long fileSizeModifier = FileSizeType.Kb.GetCustomAttribute<ValueAttribute>().Value;
+
+                    string sizeType = match.Groups["SizeType"].Value.ToUpper();
+                    if (!sizeType.IsNullOrEmpty())
+                    {
+                        fileSizeModifier = Enum.Parse<FileSizeType>(sizeType).GetCustomAttribute<ValueAttribute>().Value;
+                    }
+
+                    fileSizeMinimum = size * fileSizeModifier;
+                }
+                catch
+                {
+                    throw new Exception($"Error: could not parse filesize parameter" +
+                        $"{Environment.NewLine}Format should follow [SIZE] or [SIZE][TYPE]. Acceptable TYPE parameters are kb, mb, gb, tb" +
+                        $"{Environment.NewLine}For more information, visit https://github.com/sLill/Windows-BudgetGrep/wiki/WindowsGrep.CommandFlags");
+                }
+            }
+
+            return fileSizeMinimum;
+        }
+        #endregion GetFileSizeMinimum    
+
+        #region GetFilteredFiles
+        /// <summary>
+        /// </summary>
+        /// <param name="consoleCommand"></param>
+        /// <param name="files"></param>
+        /// <returns>Returns files filtered by Inclusion/Exclusion type parameters</returns>
+        public static List<string> GetFilteredFiles(ConsoleCommand consoleCommand, List<string> files)
+        {
+            var fileTypeFilters = GetFileTypeFilters(consoleCommand);
+            var fileTypeExcludeFilters = GetFileTypeExcludeFilters(consoleCommand);
+            var pathFilters = GetPathFilters(consoleCommand);
+            var pathExcludeFilters = GetPathExcludeFilters(consoleCommand);
+
+            // Filter files by type
+            files = fileTypeFilters == null ? files : files.Where(file => fileTypeFilters.Contains(Path.GetExtension(file).Trim('.'))).ToList();
+            files = fileTypeExcludeFilters == null ? files : files.Where(file => !fileTypeExcludeFilters.Contains(Path.GetExtension(file).Trim('.'))).ToList();
+
+            // Filter files by relative subpaths
+            files = pathFilters == null ? files : files.Where(file => pathFilters.Any(x => Regex.IsMatch(file, x))).ToList();
+            files = pathExcludeFilters == null ? files : files.Where(file => !pathExcludeFilters.Any(x => Regex.IsMatch(file, x))).ToList();
+
+            return files;
+        }
+        #endregion GetFilteredFiles
+
+        #region GetFileTypeFilters
+        public static IEnumerable<string> GetFileTypeFilters(ConsoleCommand consoleCommand)
+        {
+            bool fileTypeFilterFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.FileTypeFilter);
+            var fileTypeFilters = fileTypeFilterFlag ? consoleCommand.CommandArgs[ConsoleFlag.FileTypeFilter].Split(new char[] { ',', ';' }).Select(x => x.Trim('.')) : null;
+
+            return fileTypeFilters;
+        }
+        #endregion GetFileTypeFilters
+
+        #region GetFileTypeExcludeFilters
+        public static IEnumerable<string> GetFileTypeExcludeFilters(ConsoleCommand consoleCommand)
+        {
+            bool fileTypeExcludeFilterFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.FileTypeExcludeFilter);
+            var fileTypeExcludeFilters = fileTypeExcludeFilterFlag ? consoleCommand.CommandArgs[ConsoleFlag.FileTypeExcludeFilter].Split(new char[] { ',', ';' }).Select(x => x.Trim('.')) : null;
+
+            return fileTypeExcludeFilters;
+        }
+        #endregion GetFileTypeFilters
+
+        #region GetPathFilters
+        public static IEnumerable<string> GetPathFilters(ConsoleCommand consoleCommand)
+        {
+            bool pathFilterFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.PathFilter);
+            var pathFilters = pathFilterFlag ? consoleCommand.CommandArgs[ConsoleFlag.PathFilter].Split(new char[] { ',', ';' }) : null;
+
+            return pathFilters;
+        }
+        #endregion GetPathFilters
+
+        #region GetPathExcludeFilters
+        public static IEnumerable<string> GetPathExcludeFilters(ConsoleCommand consoleCommand)
+        {
+            bool pathExcludeFilterFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.PathExcludeFilter);
+            var pathExcludeFilters = pathExcludeFilterFlag ? consoleCommand.CommandArgs[ConsoleFlag.PathExcludeFilter].Split(new char[] { ',', ';' }) : null;
+
+            return pathExcludeFilters;
+        }
+        #endregion GetPathExcludeFilters
+        #endregion Methods..
+    }
+}
--- WindowsGrep/WindowsGrep.Engine/GrepEngine.cs
+++ WindowsGrep/WindowsGrep.Engine/GrepEngine.cs
@@ -14,10 +14,8 @@ namespace WindowsGrep.Engine
     public static class GrepEngine
     {
         #region Fields..
-        private const string FILE_SIZE_PATTERN = @"(?<Size>\d+)(?<SizeType>\S{2})?";
         private const string FILE_NAME_PATTERN = @"^(.+)[\/\\](?<FileName>[^\/\\]+)$";
 
-        private static Regex _fileSizeRegex = new Regex(FILE_SIZE_PATTERN);
         private static Regex _fileNameRegex = new Regex(FILE_NAME_PATTERN);
         private static object _searchLock = new object();
         #endregion Fields..
@@ -37,7 +35,7 @@ namespace WindowsGrep.Engine
 
             string filepath = GetPath(consoleCommand);
             List<string> files = GetFiles(consoleCommand, grepResultCollection, filepath);
-            files = GetFilteredFiles(consoleCommand, files);
+            files = ConsoleCommandUtils.GetFilteredFiles(consoleCommand, files);
 
             // Clear the result collection between chained commands so that only the results of the final command are returned
             grepResultCollection.Clear();
@@ -295,151 +293,6 @@ namespace WindowsGrep.Engine
         }
         #endregion GetFileNameMatches
 
-        #region GetFileSizeMaximum
-        private static long GetFileSizeMaximum(ConsoleCommand consoleCommand)
-        {
-            long fileSizeMaximum = -1;
-            bool fileSizeMaximumFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.FileSizeMaximum);
-
-            if (fileSizeMaximumFlag)
-            {
-                try
-                {
-                    string fileSizeMaximumParameter = consoleCommand.CommandArgs[ConsoleFlag.FileSizeMaximum];
-
-                    var match = _fileSizeRegex.Match(fileSizeMaximumParameter);
-                    long size = Convert.ToInt64(match.Groups["Size"].Value);
-
-                    if (size < 0)
-                        throw new Exception("Error: Size parameter cannot be less than 0");
-
-                    long fileSizeModifier = FileSizeType.Kb.GetCustomAttribute<ValueAttribute>().Value;
-
-                    string sizeType = match.Groups["SizeType"].Value.ToUpper();
-                    if (!sizeType.IsNullOrEmpty())
-                        fileSizeModifier = Enum.Parse<FileSizeType>(sizeType, true).GetCustomAttribute<ValueAttribute>().Value;
-
-                    fileSizeMaximum = size * fileSizeModifier;
-                }
-                catch
-                {
-                    throw new Exception($"Error: Could not parse filesize parameter" +
-                        $"{Environment.NewLine}Format should follow [SIZE] or [SIZE][TYPE]. Acceptable TYPE parameters are kb, mb, gb, tb" +
-                        $"{Environment.NewLine}For more information, visit https://github.com/sLill/Windows-BudgetGrep/wiki/WindowsGrep.CommandFlags");
-                }
-            }
-
-            return fileSizeMaximum;
-        }
-        #endregion GetFileSizeMaximum
-
-        #region GetFileSizeMinimum
-        private static long GetFileSizeMinimum(ConsoleCommand consoleCommand)
-        {
-            long fileSizeMinimum = -1;
-            bool fileSizeMinimumFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.FileSizeMinimum);
-
-            if (fileSizeMinimumFlag)
-            {
-                try
-                {
-                    string fileSizeMinimumParameter = consoleCommand.CommandArgs[ConsoleFlag.FileSizeMinimum];
-
-                    var match = _fileSizeRegex.Match(fileSizeMinimumParameter);
-                    long size = Convert.ToInt64(match.Groups["Size"].Value);
-
-                    if (size < 0)
-                    {
-                        throw new Exception("Error: Size parameter cannot be less than 0");
-                    }
-
-                    long fileSizeModifier = FileSizeType.Kb.GetCustomAttribute<ValueAttribute>().Value;
-
-                    string sizeType = match.Groups["SizeType"].Value.ToUpper();
-                    if (!sizeType.IsNullOrEmpty())
-                    {
-                        fileSizeModifier = Enum.Parse<FileSizeType>(sizeType).GetCustomAttribute<ValueAttribute>().Value;
-                    }
-
-                    fileSizeMinimum = size * fileSizeModifier;
-                }
-                catch
-                {
-                    throw new Exception($"Error: could not parse filesize parameter" +
-                        $"{Environment.NewLine}Format should follow [SIZE] or [SIZE][TYPE]. Acceptable TYPE parameters are kb, mb, gb, tb" +
-                        $"{Environment.NewLine}For more information, visit https://github.com/sLill/Windows-BudgetGrep/wiki/WindowsGrep.CommandFlags");
-                }
-            }
-
-            return fileSizeMinimum;
-        }
-        #endregion GetFileSizeMinimum
-
-        #region GetFilteredFiles
-        /// <summary>
-        /// </summary>
-        /// <param name="consoleCommand"></param>
-        /// <param name="files"></param>
-        /// <returns>Returns files filtered by Inclusion/Exclusion type parameters</returns>
-        private static List<string> GetFilteredFiles(ConsoleCommand consoleCommand, List<string> files)
-        {
-            var fileTypeFilters = GetFileTypeFilters(consoleCommand);
-            var fileTypeExcludeFilters = GetFileTypeExcludeFilters(consoleCommand);
-            var pathFilters = GetPathFilters(consoleCommand);
-            var pathExcludeFilters = GetPathExcludeFilters(consoleCommand);
-
-            // Filter files by type
-            files = fileTypeFilters == null ? files : files.Where(file => fileTypeFilters.Contains(Path.GetExtension(file).Trim('.'))).ToList();
-            files = fileTypeExcludeFilters == null ? files : files.Where(file => !fileTypeExcludeFilters.Contains(Path.GetExtension(file).Trim('.'))).ToList();
-
-            // Filter files by relative subpaths
-            files = pathFilters == null ? files : files.Where(file => pathFilters.Any(x => Regex.IsMatch(file, x))).ToList();
-            files = pathExcludeFilters == null ? files : files.Where(file => !pathExcludeFilters.Any(x => Regex.IsMatch(file, x))).ToList();
-
-            return files;
-        }
-        #endregion GetFilteredFiles
-
-        #region GetFileTypeFilters
-        private static IEnumerable<string> GetFileTypeFilters(ConsoleCommand consoleCommand)
-        {
-            bool fileTypeFilterFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.FileTypeFilter);
-            var fileTypeFilters = fileTypeFilterFlag ? consoleCommand.CommandArgs[ConsoleFlag.FileTypeFilter].Split(new char[] { ',', ';' }).Select(x => x.Trim('.')) : null;
-
-            return fileTypeFilters;
-        }
-        #endregion GetFileTypeFilters
-
-        #region GetFileTypeExcludeFilters
-        private static IEnumerable<string> GetFileTypeExcludeFilters(ConsoleCommand consoleCommand)
-        {
-            bool fileTypeExcludeFilterFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.FileTypeExcludeFilter);
-            var fileTypeExcludeFilters = fileTypeExcludeFilterFlag ? consoleCommand.CommandArgs[ConsoleFlag.FileTypeExcludeFilter].Split(new char[] { ',', ';' }).Select(x => x.Trim('.')) : null;
-
-            return fileTypeExcludeFilters;
-        }
-        #endregion GetFileTypeFilters
-
-        #region GetPathFilters
-        private static IEnumerable<string> GetPathFilters(ConsoleCommand consoleCommand)
-        {
-            bool pathFilterFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.PathFilter);
-            var pathFilters = pathFilterFlag ? consoleCommand.CommandArgs[ConsoleFlag.PathFilter].Split(new char[] { ',', ';' }) : null;
-
-            return pathFilters;
-        }
-        #endregion GetPathFilters
-
-        #region GetPathExcludeFilters
-        private static IEnumerable<string> GetPathExcludeFilters(ConsoleCommand consoleCommand)
-        {
-            bool pathExcludeFilterFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.PathExcludeFilter);
-            var pathExcludeFilters = pathExcludeFilterFlag ? consoleCommand.CommandArgs[ConsoleFlag.PathExcludeFilter].Split(new char[] { ',', ';' }) : null;
-
-            return pathExcludeFilters;
-        }
-        #endregion GetPathExcludeFilters
-
         #region GetPath
         private static string GetPath(ConsoleCommand consoleCommand)
         {
@@ -477,8 +330,8 @@ namespace WindowsGrep.Engine
         {
             bool fileNamesOnlyFlag = consoleCommand.CommandArgs.ContainsKey(ConsoleFlag.FileNamesOnly);
 
-            long fileSizeMin = GetFileSizeMinimum(consoleCommand);
-            long fileSizeMax = GetFileSizeMaximum(consoleCommand);
+            long fileSizeMin = ConsoleCommandUtils.GetFileSizeMinimum(consoleCommand);
+            long fileSizeMax = ConsoleCommandUtils.GetFileSizeMaximum(consoleCommand);
             var searchMetrics = new SearchMetrics();
 
             // Build content search pattern
