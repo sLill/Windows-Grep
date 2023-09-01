05ab7cd38a277563e1aeac5a23e00efca37c3c1d
sLill <SamuelTurnerLill@gmail.com>
2023-09-01 00:30:17 -0500

    - Refactor

--- WindowsGrep/WindowsGrep.Core/Utils/ConsoleUtils.cs
+++ WindowsGrep/WindowsGrep.Core/Utils/ConsoleUtils.cs
@@ -102,7 +102,7 @@ namespace WindowsGrep.Core
                         return (commandType, commandParameter);
                     }
                 }
-            };
+            }
 
             return default;
         }
