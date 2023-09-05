eb362e6d19e91d61fdd09a5ef76dcc23ec6e185c
sLill <SamuelTurnerLill@gmail.com>
2023-09-05 15:48:26 -0500

    - Hash tests added

--- WindowsGrep/WindowsGrep.Test/Flags/Hash/HashTests.cs
+++ WindowsGrep/WindowsGrep.Test/Flags/Hash/HashTests.cs
@@ -32,11 +32,11 @@ namespace WindowsGrep.Test.Flags.Hash
         #endregion Setup
 
         #region Tests..
-        #region Hash_FlagFirst_MD5_SingleResult
+        #region Hash_MD5_SingleResult
         [Test]
-        public async Task Hash_FlagFirst_MD5_SingleResult()
+        public async Task Hash_MD5_SingleResult()
         {
-            string testFilePath = Path.Combine(TestDataDirectory, "MD5.txt");
+            string testFilePath = Path.Combine(TestDataDirectory, "Normal.txt");
             Assert.IsTrue(File.Exists(testFilePath));
 
             string hash = "25a1568def38f50387dd63338d066f9e";
@@ -47,7 +47,75 @@ namespace WindowsGrep.Test.Flags.Hash
 
             Assert.IsTrue(commandResultCollection.Count == 1);
         }
-        #endregion Hash_FlagFirst_MD5_SingleResult
+        #endregion Hash_MD5_SingleResult       
+
+        #region Hash_SHA256_SingleResult
+        [Test]
+        public async Task Hash_SHA256_SingleResult()
+        {
+            string testFilePath = Path.Combine(TestDataDirectory, "Normal.txt");
+            Assert.IsTrue(File.Exists(testFilePath));
+
+            string hash = "aa50d2761939f9211b3d2966fa4397f75f7878b6f46a0a392076b4ae357e803d";
+            string command = $"{_flagDescriptor}0 -f '{testFilePath}' {hash}";
+
+            var commandResultCollection = new CommandResultCollection();
+            await WindowsGrep.RunCommandAsync(command, commandResultCollection, new CancellationToken());
+
+            Assert.IsTrue(commandResultCollection.Count == 1);
+        }
+        #endregion Hash_SHA256_SingleResult
+
+        #region Hash_FlagFirst
+        [Test]
+        public async Task Hash_FlagFirst()
+        {
+            string testFilePath = Path.Combine(TestDataDirectory, "Normal.txt");
+            Assert.IsTrue(File.Exists(testFilePath));
+
+            string hash = "aa50d2761939f9211b3d2966fa4397f75f7878b6f46a0a392076b4ae357e803d";
+            string command = $"{_flagDescriptor}0 -f '{testFilePath}' {hash}";
+
+            var commandResultCollection = new CommandResultCollection();
+            await WindowsGrep.RunCommandAsync(command, commandResultCollection, new CancellationToken());
+
+            Assert.IsTrue(commandResultCollection.Any());
+        }
+        #endregion Hash_FlagFirst
+
+        #region Hash_FlagMiddle
+        [Test]
+        public async Task Hash_FlagMiddle()
+        {
+            string testFilePath = Path.Combine(TestDataDirectory, "Normal.txt");
+            Assert.IsTrue(File.Exists(testFilePath));
+
+            string hash = "aa50d2761939f9211b3d2966fa4397f75f7878b6f46a0a392076b4ae357e803d";
+            string command = $"-f '{testFilePath}' {_flagDescriptor}0 {hash}";
+
+            var commandResultCollection = new CommandResultCollection();
+            await WindowsGrep.RunCommandAsync(command, commandResultCollection, new CancellationToken());
+
+            Assert.IsTrue(commandResultCollection.Any());
+        }
+        #endregion Hash_FlagMiddle
+
+        #region Hash_FlagLast
+        [Test]
+        public async Task Hash_FlagLast()
+        {
+            string testFilePath = Path.Combine(TestDataDirectory, "Normal.txt");
+            Assert.IsTrue(File.Exists(testFilePath));
+
+            string hash = "aa50d2761939f9211b3d2966fa4397f75f7878b6f46a0a392076b4ae357e803d";
+            string command = $"-f '{testFilePath}' {hash} {_flagDescriptor}0";
+
+            var commandResultCollection = new CommandResultCollection();
+            await WindowsGrep.RunCommandAsync(command, commandResultCollection, new CancellationToken());
+
+            Assert.IsTrue(commandResultCollection.Any());
+        }
+        #endregion Hash_FlagLast
         #endregion Tests..
     }
 }
