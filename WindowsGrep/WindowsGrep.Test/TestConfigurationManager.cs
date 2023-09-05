cadb14c93d294bb88b5719d36e9855057e581d2f
sLill <SamuelTurnerLill@gmail.com>
2023-09-05 16:09:37 -0500

    -

--- WindowsGrep/WindowsGrep.Test/TestConfigurationManager.cs
+++ WindowsGrep/WindowsGrep.Test/TestConfigurationManager.cs
@@ -5,18 +5,9 @@ namespace WindowsGrep.Test
 {
     public static class TestConfigurationManager
     {
-        #region Fields..
-        #endregion Fields..
-
         #region Properties..
         public static string WorkingDirectory 
             => AppDomain.CurrentDomain.BaseDirectory;
         #endregion Properties..
-
-        #region Constructors..
-        #endregion Constructors..
-
-        #region Methods..
-        #endregion Methods..
     }
 }
